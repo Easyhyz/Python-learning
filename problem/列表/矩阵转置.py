63965ff95989edb91f446649a4ad4e5e29263417
Easyhyz <h2546419945@163.com>
2026-04-08 19:28:23 +0800

    用列表推导式实现矩阵的转置

---
+++ problem/列表/矩阵转置.py
@@ -0,0 +1,24 @@
+# 正常循环实现矩阵转置
+m, n = map(int, input().split())
+
+matrix = [[0] * n for _ in range(m)] # _ 不表示任何意思，仅代表循环m次
+num = 1
+for row in range(m): # 自增
+    for col in range(n):
+        matrix[row][col] = num
+        num += 1
+
+print("原矩阵：")
+for row in range(m):
+    for col in range(n):
+        print(matrix[row][col], end='  ')
+    print()
+print()
+transpoted = []
+for i in range(n):
+    transpoted.append([row[i] for row in matrix])
+print("转置矩阵：")
+for row in range(n):
+    for col in range(m):
+        print(transpoted[row][col], end='  ')
+    print()
