f2c34d851068d9c3fe66a33764c424acadddb566
Easyhyz <h2546419945@163.com>
2026-04-25 14:46:13 +0800

    使用xpinyin模块实现提取拼音

--- problem/function/拼音回文数.py
+++ problem/function/拼音回文数.py
@@ -4,18 +4,42 @@
 from xpinyin import Pinyin
 
 # 功能：读取输入并判断字数
-# 输出：符合大于一的奇数个字的字符串
+# 输出：符合大于一的奇数个字的字符串 不必奇数
 def str_in():
-    pass
+    str_word = input("请输入一段话：(输入exit退出)")
+    while len(str_word) <= 1: # 简化代码
+        str_word = input("字数太少, 请重新输入: ")
+
+    return str_word
 
 # 功能：汉字转换成拼音
 # 输出：字符串首字母构成的序列
-def pinyin_transform(str_words):
-    pass
+def py_transf(str_words):
+    P = Pinyin()
+    pinyin_list = list(P.get_initials(str_words, '')) # 提取首字母
+
+    return pinyin_list
 
 # 功能：判断回文数
-# 输出：True or False
-def hui():
-    pass
+# 输出：True/False
+def hui(_list):
+    r_list = list(reversed(_list))
+    if r_list == _list:
+        return True
+    else:
+        return False
+    
 
 # 主程序
+str_word = str_in()
+while str_word != 'exit':
+    py = py_transf(str_word)
+    if hui(py):
+        print(f"[{str_word}]是回文。")
+    else:
+        print(f"[{str_word}]不是回文。")
+    print()
+
+    str_word = str_in()
+
+print("退出成功~")
